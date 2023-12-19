483f56e847907ff9fbbc11f5b2f919b5e63d21d2
marwasabon <marwasabon@gmail.com>
2023-12-19 16:31:14 +0000

    this fix 7

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
                              ForeignKey("places.id"),
                              primary_key=True,
-                             nullable=False))
+                             nullable=False),
                       Column("amenity_id", String(60),
                              ForeignKey("amenities.id"),
                              primary_key=True,
