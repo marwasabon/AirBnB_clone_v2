184f1ba83a08b81bf6ccc1a6c9f2f9816a260f5b
marwasabon <marwasabon@gmail.com>
2023-12-19 16:23:18 +0000

    this fix 5

--- models/place.py
+++ models/place.py
@@ -9,17 +9,6 @@ import models
 
 metadata = Base.metadata
 
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
-
-
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -64,6 +53,15 @@ class Place(BaseModel, Base):
         return (review_list)
         '''
     #reviews = relationship('Review', back_populates='place')
+    place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
