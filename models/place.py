c22deea367708eee922531e242b33c9273fc119c
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-12-23 22:46:34 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,105 +1,83 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""This is the place class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Table, Float
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
-metadata = Base.metadata
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    
+    """This is the class for Place
+    Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    #city = relationship('City', back_populates='places')
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
-    reviews = relationship('Review', back_populates='place')
-    place_amenity = Table(
-        'place_amenity',
-        metadata,
-        Column(
-            'place_id',
-            String(60),
-            ForeignKey('places.id'),
-            primary_key=True
-        ),
-        Column('amenity_id',
-               String(60),
-               ForeignKey('amenities.id'),
-               primary_key=True
-               )
-    )
-    
-    '''
-    #city_id = ""
-    #user_id = ""
-    #name = ""
-    #description = ""
-    #number_rooms = 0
-    #number_bathrooms = 0
-    #max_guest = 0
-    #price_by_night = 0
-    #latitude = 0.0
-    #longitude = 0.0
-    #amenity_ids = []
-    '''
-    '''
-    @property
-    def reviews(self):
-        returns the list of review instances with
-        place_id equal the current place.id
-        cur_id = self.id
-        review_list = []
-        objs = storage.all('Review')
-        for k, v in obj.items():
-            if v.place_id == cur_id:
-                review_list.append(str(v))
-        return (review_list)
-        '''
-    '''reviews = relationship('Review', back_populates='place')
-    place_amenity = Table(
-        'place_amenity',
-        metadata,
-        Column(
-            'place_id',
-            String(60),
-            ForeignKey('places.id'),
-            primary_key=True
-        ),
-        Column('amenity_id',
-               String(60),
-               ForeignKey('amenities.id'),
-               primary_key=True
-               )
-    )
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship(
-            'Review',
-            backref='place',
-            cascade='all, delete'
-        )
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
 
-        amenities = relationship(
-            'Amenity',
-            secondary=place_amenity,
-            viewonly=False
-        )
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         @property
         def reviews(self):
-            objs = models.storage.all(Review)
-            return [v for k, v in obj.items() if v.place_id == self.id]
-    '''
+            """ Returns list of reviews.id """
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
+
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
