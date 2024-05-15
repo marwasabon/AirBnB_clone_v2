3cc18929eb0b707396f8f565d05f357a98125e87
marwasabon <marwasabon@gmail.com>
2024-05-15 20:49:45 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+# Nginx Server Configuration.
+DR="/data/web_static"
+DEST="/etc/nginx"
+Static="/var/www/default"
+echo "server  {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        server_name localhost;
+        error_page 404 /page_not_found.html;
+        root $Static;
+
+        location / {
+                index index.html index.html;
+        }
+
+        location /hbnb_static/ {
+                alias $DR/current/;
+        }
+
+}" > "$DEST/sites-available/default"
+service nginx restart
