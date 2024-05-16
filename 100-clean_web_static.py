d248409e78ee5da3afd1666c347358bea58fe84a
marwasabon <marwasabon@gmail.com>
2024-05-16 14:10:16 +0000

    task 4

---
+++ 100-clean_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import os
+from fabric.api import *
+
+
+env.hosts = ["54.237.44.169", "34.204.95.33"]
+
+
+def do_clean(number=0):
+    """Function that delete out-of-date archive
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
